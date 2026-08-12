d92786ecf8c7c161966a25bcc9c8835c9f0d18a4
Rumia Channel <github@rumia-ch.uk>
2026-08-12 15:20:22 +0900

    Preserve explicit null storage parents

--- src/domain/storage_location.rs
+++ src/domain/storage_location.rs
@@ -1,4 +1,4 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Deserializer, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
 pub struct StorageLocation {
@@ -16,5 +16,14 @@ pub struct CreateStorageLocation {
 #[derive(Debug, Clone, Deserialize, PartialEq, Eq)]
 pub struct UpdateStorageLocation {
     pub name: Option<String>,
+    #[serde(default, deserialize_with = "deserialize_optional_optional")]
     pub parent_id: Option<Option<i64>>,
+}
+
+fn deserialize_optional_optional<'de, D, T>(deserializer: D) -> Result<Option<Option<T>>, D::Error>
+where
+    D: Deserializer<'de>,
+    T: Deserialize<'de>,
+{
+    Option::<T>::deserialize(deserializer).map(Some)
 }
